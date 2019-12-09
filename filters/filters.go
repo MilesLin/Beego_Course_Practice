d46651a1fd58d65073497f4b39527d25778888d2
Miles <milesasp@gmail.com>
2019-12-09 22:24:23 +0800

    Override from answer

--- filters/filters.go
+++ filters/filters.go
@@ -2,21 +2,22 @@ package filters
 
 import (
 	"fmt"
+
 	"github.com/astaxie/beego/context"
 )
 
-func BeforeRouterFilter(ctx *context.Context){
+func BeforeRouterFilter(ctx *context.Context) {
 	fmt.Println("Before Router filter")
 }
 
-func BeforeExecFilter(ctx *context.Context){
+func BeforeExecFilter(ctx *context.Context) {
 	fmt.Println("Before Exec filter")
 }
 
-func AfterExecRouter(ctx *context.Context){
+func AfterExecFilter(ctx *context.Context) {
 	fmt.Println("After Exec filter")
 }
 
-func FinishRouterFilter(ctx *context.Context){
+func FinishRouterFilter(ctx *context.Context) {
 	fmt.Println("Finish Router filter")
 }
--- main.go
+++ main.go
@@ -1,25 +1,20 @@
 package main
 
 import (
-	_ "lastchance/routers"
 	"lastchance/filters"
+	_ "lastchance/routers"
 
 	"github.com/astaxie/beego"
 )
 
 func main() {
 	beego.InsertFilter("/lifecycle", beego.BeforeRouter,
-	filters.BeforeRouterFilter)
-	beego.InsertFilter("/lifecycle", beego.BeforeExec,
-	filters.BeforeExecFilter)
-
-	beego.InsertFilter("/lifecycle", beego.AfterExec,
-	filters.AfterExecRouter)
-
-	beego.InsertFilter("/lifecycle", beego.FinishRouter,
-	filters.FinishRouterFilter)
+		filters.BeforeRouterFilter)
+	beego.InsertFilter("/lifecycle", beego.BeforeExec, filters.BeforeExecFilter)
+	beego.InsertFilter("/lifecycle", beego.AfterExec, filters.AfterExecFilter)
+	beego.InsertFilter("/lifecycle", beego.FinishRouter, filters.FinishRouterFilter)
 
 	beego.SetStaticPath("/public", "static")
 	beego.DelStaticPath("/static")
-	beego.Run()	
+	beego.Run()
 }
